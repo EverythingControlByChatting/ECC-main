a3b019d0f845894c321e203e2851fecd8b1fb166
5pecia1 <5o1.3an9@gmail.com>
2017-09-13 23:37:33 +0900

    make short_delay_message for example

--- sample/urls.py
+++ sample/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^wiki/slash-commands$', views.wiki_slash_commands, name='wiki'),
+    url(r'^short-delay-message/slash-commands$', views.short_delay_message, name='short-delay-message'),
 ]
--- sample/views.py
+++ sample/views.py
@@ -1,7 +1,10 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-from ECC_main.response import SlashResponse
+from ECC_main.response import SlashResponse, LazySlashResponse
+
+import time
+import re
 
 
 @csrf_exempt
@@ -14,3 +17,47 @@ def wiki_slash_commands(request):
         "검색한 단어 : *" + code + "*\n"
         + "> https://ko.wikipedia.org/wiki/" + code
     )
+
+
+@csrf_exempt
+@require_POST
+def short_delay_message(request):
+    COMMAND_REGEX = '(?P<second>\d+([.]\d)?)[,]\s?(?P<message>.+)'
+
+    p = re.compile(COMMAND_REGEX, re.DOTALL)
+
+    text = request.POST['text']
+    response_url = request.POST['response_url']
+    user_id = request.POST['user_id']
+    user_name = request.POST['user_name']
+
+    print(text)
+
+    if p.match(text) is None:
+        return SlashResponse("유요하지 않은 명령입니다.")
+
+    m = p.search(text)
+
+    delay_time = float(m.group("second"))
+    message = m.group("message")
+
+    if delay_time > 60 * 30:
+        return SlashResponse("유요하지 않은 시간입니다.\n30분 이내로 설정해 주십시오")
+
+    def delay_message():
+        time.sleep(delay_time)
+        return "_<@" + user_id + "|" + user_name + ">님이 생산한 메시지_\n\n" + message
+
+    waiting_message = "*" + str(delay_time) + "* 초 뒤에\n```" + \
+                      message + "```\n가 전달됩니다."
+
+    return LazySlashResponse(
+        response_url, delay_message,
+        request_result_func=showRequest,
+        waiting_message=waiting_message,
+        response_type="in_channel"
+    )
+
+
+def showRequest(request):
+    print(request)
