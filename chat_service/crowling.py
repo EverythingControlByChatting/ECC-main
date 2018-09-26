3a13dd3437ab933b2451fe60251e2880e8da353c
Donghun Lim <odh621@likelion.org>
2018-09-26 19:14:10 +0000

    chat_service Telegram 연동 및 크롤링 업데이트

--- chat_service/crowling.py
+++ chat_service/crowling.py
@@ -8,19 +8,21 @@ def crowlier_lunch(context):
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
 
-    list_name = soup.select(".dc-restaurant-name")
-    list_info = soup.select(".dc-restaurant-category")
-    list_address = soup.select("dc-restaurant-info")
+    list_name = soup.select(".localeft-cont > .lc-list > #div_list > li > a > .btxt")
+    list_info = soup.select(".localeft-cont > .lc-list > #div_list > li > a > .stxt")
+    list_address = soup.select(".localeft-cont > .lc-list > #div_list > li > a > span:nth-of-type(5)")
     lists_name = []
     for list1 in list_name:
         lists_name.append(list1.text.strip('\n'))
+    print(list_address)
+    
     address = []
     for addr in list_address:
-        address.append(addr.select('.dc-restaurant-info')[1])
+        address.append(addr.text.strip())
     
     lists_address = []
     for addr in address:
-        data = addr.select('.dc-restaurant-info-text')[0].text
+        data = addr
         lists_address.append("http://map.daum.net/?q="+coordinate.get_address(data))
    
     lists_info = []
@@ -30,15 +32,17 @@ def crowlier_lunch(context):
 
     for i in range(0, len(lists_info)):        
         lists_name_info.append(lists_name[i]+"("+lists_info[i]+")")
+    print(lists_name_info)    
     return lists_name_info, lists_address
-
+    
 def lunch_category():
     req = requests.get('http://www.diningcode.com/isearch.php?')
 
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
 
-    list_category = soup.select(".dc-search-food-category-list-item")
+    list_category = soup.select(".category-menu > .list > li > a")
+    print(list_category)
     lists_category = []
     for i in range(0, len(list_category)):
         lists_category.append(list_category[i].text)
--- chat_service/views.py
+++ chat_service/views.py
@@ -1,51 +1,118 @@
 from django.shortcuts import render
-from ECC_main.request import slack_slash_request
+from ECC_main.request import slack_slash_request, slash_request
 from ECC_main.response import SlashResponse
+from ECC_main.platform.telegram import Telegram 
 from . import crowling
 import json
 
-@slack_slash_request
+
+@slash_request
 def lunch(request):
-    text = request.POST['text']
+    text = request['text']
     respon= crowling.crowlier_lunch(text)
     respon1 = list(zip(respon[0], respon[1]))
     list1 = []
 
-    for (first, last) in respon1:
+    if request['platform'] is Telegram.platform():
+        for (first, last) in respon1:
+            list1.append(first)
+            list1.append("주소 : " + last)
+            
+        test = str(list1)
+        count = 0
+        number = 0 
+        list_str = []
+        
+        for mark in test:
+            if test[number] == ',':
+                count = count + 1
+            if count == 3:
+                if test[number] == ',':
+                    list_str.append('\n')
+            if count == 4:
+                if test[number] == ',':
+                    list_str.append('\n')
+                    count = 0
+            else:    
+                list_str.append(test[number])
+            number = number + 1
+        
+        test = "".join(list_str)
+        test = str(test)
+        
+        result = "*맛집 결과?*\n\n"
+        result = result + test
+        slashResponse = SlashResponse(
+            result
+        )
+        
+    else:
+        
+      for (first, last) in respon1:
         list1.append({
         'title': first,
         'value': last,
         'short': True
+      })        
+        
+      slashResponse = SlashResponse({
+        "attachments": [
+            {
+                "title": text+" 맛집입니다.",
+                "fields":list1,
+                "color": "#F35A00",
+            }
+        ]
     })
+    return slashResponse;
 
-    return SlashResponse({
-    "attachments": [
-        {
-            "title": text+" 맛집입니다.",
-            "fields":list1,
-            "color": "#F35A00",
-        }
-    ]
-})
-
-@slack_slash_request
+@slash_request
 def lunch_category(request):
-    text = request.POST['text']
+    text = request['text']
     respon= crowling.lunch_category()
     
     list1 = []
-
-    for first in respon:
-        list1.append({
-        'title': first,
-        'short': True
+        
+    if request['platform'] is Telegram.platform():
+        for first in respon:
+            if first != '전체보기':
+                list1.append(" "+ first + " ")
+            
+        test = str(list1)
+        count = 0
+        number = 0 
+        list_str = []
+        
+        for mark in test:
+            if test[number] == ',':
+                list_str.append('\n')
+            else:    
+                list_str.append(test[number])
+            number = number + 1
+        
+        test = "".join(list_str)
+        test = str(test)    
+        
+        result = "*맛집 카테고리*\n\n"
+        result = result + test
+        print(result)
+        slashResponse = SlashResponse(
+            result
+        )
+        return slashResponse
+    else:
+        
+        for first in respon:
+            list1.append({
+            'title': first,
+            'short': True
+        })
+        return SlashResponse({
+        "attachments": [
+            {
+                "title": "맛집 카테고리입니다",
+                "fields": list1,
+                "color": "#F35A00",
+            }
+        ]
     })
-    return SlashResponse({
-    "attachments": [
-        {
-            "title": "맛집 카테고리입니다",
-            "fields": list1,
-            "color": "#F35A00",
-        }
-    ]
-})
